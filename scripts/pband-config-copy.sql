3125f36081734f31a342df680134a91dbea64bab
Jack O'Connor <j.oconnor@yale.edu>
2024-02-20 11:15:47 -0500

    Modifications and upgrades

---
+++ scripts/pband-config-copy.sql
@@ -0,0 +1,8 @@
+BEGIN TRAN
+
+INSERT INTO PerformanceBandConfiguration (DistrictID,StateID,Year,Season,DataSetCategoryID,Subject,Grade,Keyword,ScoreTier,SubscoreName,ScoreName,PerformanceBandGroupID,Cutoffs,IsPrincipalLevel,CreatedDate,UpdatedDate)
+SELECT @districtID,StateID,Year,Season,DataSetCategoryID,Subject,Grade,Keyword,ScoreTier,SubscoreName,ScoreName,PerformanceBandGroupID,Cutoffs,IsPrincipalLevel,CreatedDate,UpdatedDate
+FROM PerformanceBandConfiguration
+WHERE -- set conditions
+
+ROLLBACK TRAN
