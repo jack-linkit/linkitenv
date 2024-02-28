6207c9a394177469030388609316393e25caec7b
JackWork <jack@linkit.com>
2024-02-28 16:37:36 -0500

    removed -n from sed commands

--- scripts/bash/user_errors.sh
+++ scripts/bash/user_errors.sh
@@ -1,1 +1,1 @@
-sed -n "s/.*TeacherCode (\(.*\)) associated with a different TeacherName (\(.*\)) than provided (\(.*\))/('\1','\3')/"
+sed "s/.*TeacherCode (\(.*\)) associated with a different TeacherName (\(.*\)) than provided (\(.*\))/('\1','\3')/"
