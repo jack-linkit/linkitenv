e9466958a24956ec8ba1604a16babb7cf60c1a49
JackWork <jack@linkit.com>
2024-08-06 10:04:06 -0400

    duplicate student identification scripts

---
+++ scripts/sql/all-altcode-dupes.sql
@@ -0,0 +1,70 @@
+/* Script to identify pairs of duplicate students (AltCode) */
+-- DECLARE @DistrictID int = 4760
+
+-- all students in district, counting instances of AltCode
+DECLARE @data TABLE (StudentID INT, AltCode VARCHAR(100),DistrictID INT, row_number INT, FullName varchar(200))
+INSERT INTO @data
+SELECT 
+    StudentID,
+    AltCode,
+    Student.DistrictID,
+    ROW_NUMBER() OVER (PARTITION BY Student.DistrictID, AltCode ORDER BY CASE WHEN ModifiedUser = 2 THEN 0 ELSE 1 END, StudentID), -- prefer roster loaded students
+    FullName
+FROM
+    Student WITH (NOLOCK)
+JOIN
+    District WITH (NOLOCK)
+ON
+    Student.DistrictID = District.DistrictID
+JOIN
+    DistrictMeta WITH (NOLOCK)
+ON
+    District.DistrictID = DistrictMeta.DistrictID 
+    AND DistrictMeta.Name = 'DistrictType' and DistrictMeta.Data = 'DTS'
+WHERE 
+    LEN(Student.AltCode) > 1
+    AND District.StateID < 75
+    AND ISNUMERIC(Student.AltCode) = 1
+    -- AND Student.DistrictID = @DistrictID
+
+;WITH Originals AS (
+    SELECT 
+        StudentID,
+        AltCode,
+        FullName,
+        DistrictID
+    FROM 
+        @data
+    WHERE row_number = 1
+),
+Duplicates AS (
+    SELECT 
+        StudentID,
+        AltCode,
+        FullName,
+        DistrictID
+    FROM 
+        @data
+    WHERE row_number > 1
+)
+
+-- duplicates only
+SELECT
+    o.StudentID AS 'Orig StudentID',
+    o.FullName, 
+    d.StudentID AS 'Dupe StudentID',
+    d.FullName,
+    o.AltCode,
+	district.Name
+FROM 
+    Originals o
+JOIN
+    Duplicates d
+ON
+    o.AltCode = d.AltCode
+	and o.DistrictID = d.DistrictID
+    -- and o.FullName = d.FullName
+JOIN 
+	District with (nolock)
+ON
+	d.DistrictID = district.DistrictID
